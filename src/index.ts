2f444bae317b10a083f860fb1ca25ce095133e55
Jake Stäzrad <jakewebbsite@gmail.com>
2021-05-12 00:40:51 -0500

    webpack builds

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import iframeResizer from 'iframe-resizer'
+import { WidgetConfig } from './types'
+
+export function init () {
+  const elements = document.querySelectorAll('a.trekko-widget')
+
+  elements.forEach(element => {
+    const organizationId = element.attributes.getNamedItem('data-org-id')?.value
+
+    if (!organizationId) return console.warn('<a> element must have data-org-id to load Trekko!')
+
+    embed({ organizationId }, element)
+  })
+}
+init()
+
+export function embed (config: WidgetConfig, element: Element) {
+  element.innerHTML = ''
+
+  const url = `https://trekko.app/widget/trail-list?organizationId=${config.organizationId}`
+  const style = `display:block;width:100%;border:0px;background-color:black;`
+  const iframe = document.createElement('iframe')
+
+  iframe.setAttribute('src', url)
+  iframe.setAttribute('style', style)
+
+  element.appendChild(iframe)
+  iframeResizer({}, iframe)
+}
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export type WidgetConfig = {
+  organizationId: string
+}
